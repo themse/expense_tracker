511a1316a0010bb2d038ff1a7c3e13332f35bc71
Den Polusmyak <den.polusmyak@gmail.com>
2024-02-03 03:11:47 +0200

    feat: update expense model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,15 +2,20 @@ import 'package:uuid/uuid.dart';
 
 const Uuid _uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
 
+  final Category category;
+
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = _uuid.v4();
 }
